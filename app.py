95dc6e698be82daa07eb6922ae3dcf8294313bba
Tanmay Shingavi <126552514+decodingafterlife@users.noreply.github.com>
2024-12-01 21:48:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def main():
     Tired of always wondering what other creatures having a eerie shaped muscle as their 
     processing unit 🧠, are thinking about your outfit? Don't worry Mr Chipper got you covered,
     he'll give an honest opinion on your fashion without any discrimination or lies. 📢 
-    With his multiple personalities we have got all angles covered!
+    With his multiple different personalities we have got all angles covered!
     """)
     
     # Style selector
